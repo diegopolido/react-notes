ab43404a259c981c81dd6f40b8939e3e7b5ca176
Diego Polido Santana <diego@polidosantana.com.br>
2018-02-14 18:32:20 -0200

    Adding data to localStorage and migrate note styles to data objects

--- src/Board.js
+++ src/Board.js
@@ -5,31 +5,36 @@ import FaPlus from 'react-icons/lib/fa/plus'
 class Board extends Component {
 	constructor(props) {
 		super(props)
+		const notes = localStorage.getItem("notes") ? JSON.parse(localStorage.getItem("notes")) : [];
 		this.state = {
-			notes: []
+			notes: notes
 		}
 		this.add = this.add.bind(this)
 		this.eachNote = this.eachNote.bind(this)
 		this.update = this.update.bind(this)
 		this.remove = this.remove.bind(this)
 		this.nextId = this.nextId.bind(this)
+		this.getNewStyle = this.getNewStyle.bind(this)
 	}
 
-	add(text) {
+	add(text, style) {
 		this.setState(prevState => ({
 			notes: [
 				...prevState.notes,
 				{
 					id: this.nextId(),
-					note: text
+					note: text,
+					style: style
 				}
 			]
 		}))
 	}
 
 	nextId() {
-		this.uniqueId = this.uniqueId || 0
-		return this.uniqueId++
+		this.uniqueId = localStorage.getItem("uniqueId") ? parseInt(localStorage.getItem("uniqueId")) : 0
+		this.uniqueId++
+		localStorage.setItem("uniqueId", this.uniqueId)
+		return this.uniqueId
 	}
 
 	update(newText, i) {
@@ -51,7 +56,8 @@ class Board extends Component {
 	eachNote(note, i) {
 		return (
 			<Note key={note.id}
-				  index={note.id}
+					index={note.id}
+					style={note.style}
 				  onChange={this.update}
 				  onRemove={this.remove}>
 				  {note.note}
@@ -59,11 +65,24 @@ class Board extends Component {
 		)
 	}
 
+	getNewStyle() {
+		return {
+			right: this.randomBetween(0, window.innerWidth - 150, 'px'),
+			top: this.randomBetween(0, window.innerHeight - 150, 'px'),
+			transform: `rotate(${this.randomBetween(-25, 25, 'deg')})`
+		}
+	}
+
+	randomBetween(x, y, s) {
+		return x + Math.ceil(Math.random() * (y-x)) + s
+	}
+
 	render() {
+		localStorage.setItem("notes", JSON.stringify(this.state.notes))
 		return (
 			<div className="board">
 				{this.state.notes.map(this.eachNote)}
-				<button onClick={this.add.bind(null, "New Note")}
+				<button onClick={this.add.bind(null, "New Note", this.getNewStyle())}
 						id="add">
 					<FaPlus />
 				</button>
--- src/Note.js
+++ src/Note.js
@@ -14,19 +14,6 @@ class Note extends Component {
 		this.save = this.save.bind(this)
 		this.renderForm = this.renderForm.bind(this)
 		this.renderDisplay = this.renderDisplay.bind(this)
-		this.randomBetween = this.randomBetween.bind(this)
-	}
-
-	componentWillMount() {
-		this.style = {
-			right: this.randomBetween(0, window.innerWidth - 150, 'px'),
-			top: this.randomBetween(0, window.innerHeight - 150, 'px'),
-			transform: `rotate(${this.randomBetween(-25, 25, 'deg')})`
-		}
-	}
-
-	randomBetween(x, y, s) {
-		return x + Math.ceil(Math.random() * (y-x)) + s
 	}
 
 	componentDidUpdate() {
@@ -65,7 +52,7 @@ class Note extends Component {
 
 	renderForm() {
 		return (
-			<div className="note" style={this.style}>
+			<div className="note" style={this.props.style}>
 				<form onSubmit={this.save}>
 					<textarea ref={input => this._newText = input}
 							  defaultValue={this.props.children}/>
@@ -77,7 +64,7 @@ class Note extends Component {
 
 	renderDisplay() {
 		return (
-			<div className="note" style={this.style}>
+			<div className="note" style={this.props.style}>
 				<p>{this.props.children}</p>
 				<span>
 					<button onClick={this.edit} id="edit"><FaPencil /></button>
